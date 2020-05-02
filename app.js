670c216a80c874dc14281e804ff1b62d402a3eb8
Winsome Yuen <winsomeyuen@hotmail.com>
2020-05-02 16:31:09 +0100

    feat(app): adding additional information
    fixed css formatting for better ux

--- app.js
+++ app.js
@@ -11,13 +11,45 @@ document.addEventListener('DOMContentLoaded', () => {
 
         response.json().then(data => {
           const name = data[0].name
+          const tagline = data[0].tagline
           const description = data[0].description
+          const abv = data[0].abv
+          const ibu = data[0].ibu
+          const hops = data[0].ingredients.hops
           const food = data[0].food_pairing
           const image = data[0].image_url
 
           beerName.innerHTML = name
           beerDescription.innerHTML = description
+          beerAbv.innerHTML = abv
+          beerIbu.innerHTML = ibu
+          beerTagline.innerHTML = '"'+tagline+'"'
 
+          //formatting the hops into a single line string
+          var beerHopsArray = []
+          console.log(hops)
+          for (var i = 0; i < hops.length; i++) {
+            var hopName = hops[i].name
+            if(!(beerHopsArray.indexOf(hopName) > -1)){
+              beerHopsArray.push(hopName)
+            }
+          }
+
+          var hopsNames = ''
+          for(var i = 0; i < beerHopsArray.length; i++){
+            hopsNames += beerHopsArray[i] + ", "
+          }
+          beerHops.innerHTML = (hopsNames.substring(0, hopsNames.length - 2))
+
+          //creating a list for food pairing
+          var foodPairing = '<ul>'
+          for (var i = 0; i < food.length; i++) {
+            foodPairing += '<li>'+ food[i] +'</li>'
+          }
+          foodPairing += '</ul>'
+          beerPairing.innerHTML = foodPairing
+
+          //checking if image exists
           if(image != null){
             document.getElementById('beerImage').src = image
           } else {
